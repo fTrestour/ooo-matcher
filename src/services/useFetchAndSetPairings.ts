edb69f4908baeead8e44c88d410548ec23d48ac4
Elie Dutheil <eliedthl@gmail.com>
2021-04-30 16:12:45 +0200

    :bug: Update the way constraints are computed from sheet
    This solves multiple issues:
    
    - When anybody is selected as wish
    - When somebody is selected as wish but did not ask to participate
    - When nobody is selected as whish which mean the person wants to skip the round

--- src/services/useFetchAndSetPairings.ts
+++ src/services/useFetchAndSetPairings.ts
@@ -1,7 +1,7 @@
 import { isAfter, startOfWeek } from 'date-fns';
 import { useEffect } from 'react';
 import { ListActions } from 'react-use/lib/useList';
-import { Constraint, PreferConstraintInterface } from './Constraint';
+import { Constraint } from './Constraint';
 import { Person } from './Person';
 
 const GOOGLE_SHEET_PAIRINGS_URL =
@@ -10,6 +10,8 @@ const GOOGLE_SHEET_PAIRINGS_URL =
 interface GoogleSheetData {
   values: Array<[Person, Person, Person, string]>;
 }
+
+const ANYBODY_CHOICE = 'Anybody';
 
 export const useFetchAndSetPairings = (
   editPersons: ListActions<Person>,
@@ -34,34 +36,56 @@ const setPairings = (
       creationDate && isAfter(new Date(creationDate), startOfWeek(new Date())),
   );
 
-  const newPersons = new Set<Person>();
-  const newConstraints = new Array<PreferConstraintInterface>();
+  const newPersons = new Array<Person>();
   for (const choice of weekPairings) {
-    const [personFullName, firstWish, secondWish] = choice;
+    const [personFullName] = choice;
 
     const person = getFirstName(personFullName);
-
-    newPersons.add(person);
-    newPersons.add(firstWish);
-    newPersons.add(secondWish);
-
-    newConstraints.push({
-      subject: person,
-      type: 'wants to be matched with',
-      target: firstWish,
-      round: 1,
-    });
-
-    newConstraints.push({
-      subject: person,
-      type: 'wants to be matched with',
-      target: secondWish,
-      round: 2,
-    });
+    newPersons.push(person);
   }
 
-  editPersons.set(Array.from(newPersons));
+  const newConstraints = new Array<Constraint>();
+  for (const choice of weekPairings) {
+    const [personFullName, firstWish, secondWish] = choice;
+    const person = getFirstName(personFullName);
+
+    addConstraint(newConstraints, newPersons, person, firstWish, 1);
+    addConstraint(newConstraints, newPersons, person, secondWish, 2);
+  }
+
+  editPersons.set(newPersons);
   editConstraints.set(newConstraints);
 };
 
 const getFirstName = (name: string) => name.split(' ')[0]!;
+
+const addConstraint = (
+  constraints: Array<Constraint>,
+  availablePersons: Array<Person>,
+  person1: Person,
+  person2: Person | undefined,
+  round: 1 | 2,
+) => {
+  if (!person2) {
+    return constraints.push({
+      subject: person1,
+      type: 'will be missing during',
+      round,
+    });
+  }
+
+  if (person2.includes(ANYBODY_CHOICE)) {
+    return;
+  }
+
+  if (!availablePersons.includes(person2)) {
+    return;
+  }
+
+  return constraints.push({
+    subject: person1,
+    type: 'wants to be matched with',
+    target: person2,
+    round,
+  });
+};
